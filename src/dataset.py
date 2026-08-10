4c588b6b80af02e2e6e81d7810cc3b20f1ca957a
Henil Parmar <hyparmar01@gmail.com>
2026-08-10 13:03:14 +0530

    Add data augmentation

--- src/dataset.py
+++ src/dataset.py
@@ -1,5 +1,8 @@
 import torch
 from torch.utils.data import Dataset
+import random
+import numpy as np
+from scipy.ndimage import rotate
 
 from load_data import(
     get_patient_folders,
@@ -43,6 +46,36 @@ class BrainTumorDataset(Dataset):
         image = image[:, :, slice_index, :]
         mask = mask[:, :, slice_index]
 
+        # data augmantation
+
+        if random.random() < 0.5:
+            image = np.fliplr(image).copy()
+            mask = np.fliplr(mask).copy()
+
+        if random.random() < 0.5:
+            angle = random.uniform(-10, 10)
+
+            image = rotate(
+                image,
+                angle,
+                axes=(0, 1),
+                reshape=False,
+                order=1,
+                mode='constant',
+                cval=0
+            )
+
+            mask = rotate(
+                mask,
+                angle,
+                axes=(0, 1),
+                reshape=False,
+                order=0,
+                mode='constant',
+                cval=0
+            )
+
+            
         #Conv2D accept (channel, height, width) but we have (height, width, channel)
         image = image.transpose(2, 0, 1)
 
@@ -53,10 +86,32 @@ class BrainTumorDataset(Dataset):
 
 
 def main():
-    pass
-   
 
-    
+    # Get patient folders
+    patient_folders = get_patient_folders()
+
+    print("Total patients:", len(patient_folders))
+
+    # Create dataset
+    dataset = BrainTumorDataset(patient_folders)
+
+    print("Total samples:", len(dataset))
+
+    # Get one sample
+    image, mask = dataset[75]
+
+    print("Image shape:", image.shape)
+    print("Mask shape:", mask.shape)
+
+    print("Image dtype:", image.dtype)
+    print("Mask dtype:", mask.dtype)
+
+    print("Image min:", image.min().item())
+    print("Image max:", image.max().item())
+
+    print("Mask min:", mask.min().item())
+    print("Mask max:", mask.max().item())
+
+
 if __name__ == "__main__":
     main()
-
